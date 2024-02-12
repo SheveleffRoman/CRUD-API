dbe7006d9adedb8c18a86403e8ccfcad15586275
SheveleffRoman <@>
2024-02-13 01:28:10 +0400

    feat: add messages and codes constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGES = {
+  INVALID_ID: { message: "Not valid user id" },
+  MISSING_FIELDS: { message: "Missing required fields" },
+  USER_NOT_FOUND: { message: "User not found" },
+  RESOURCE_NOT_EXIST: { message: "Resource doesn`t exist" },
+  USER_SUCCESSFULLY_DELETE: { message: "User has been deleted" },
+  INTERNAL_SERVER_ERROR_MESSAGE: { message: "Internal Server Error" },
+};
---
+++ src/constants/statusCodes.ts
@@ -0,0 +1,7 @@
+export enum HTTP_STATUS_CODES {
+  OK = 200,
+  CREATED = 201,
+  NOT_FOUND = 404,
+  BAD_REQUEST = 400,
+  NO_CONTENT = 204,
+}
